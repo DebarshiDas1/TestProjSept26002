3ff58722bcdebcc0581d0f06b9f0980ba354d2c9
DebarshiDas1 <94341651+DebarshiDas1@users.noreply.github.com>
2024-09-26 19:13:12 +0530

    Upload files from CodeZen

---
+++ API/Entities/ExplanationOfBenefits.g.cs
@@ -0,0 +1,64 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace TestProjSept26002.Entities
+{
+#pragma warning disable
+    /// <summary> 
+    /// Represents a explanationofbenefits entity with essential details
+    /// </summary>
+    public class ExplanationOfBenefits
+    {
+        /// <summary>
+        /// TenantId of the ExplanationOfBenefits 
+        /// </summary>
+        public Guid? TenantId { get; set; }
+
+        /// <summary>
+        /// Primary key for the ExplanationOfBenefits 
+        /// </summary>
+        [Key]
+        [Required]
+        public Guid Id { get; set; }
+        /// <summary>
+        /// Code of the ExplanationOfBenefits 
+        /// </summary>
+        public string? Code { get; set; }
+        /// <summary>
+        /// Name of the ExplanationOfBenefits 
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// PaymentDate of the ExplanationOfBenefits 
+        /// </summary>
+        [Column(TypeName = "datetime")]
+        public DateTime? PaymentDate { get; set; }
+        /// <summary>
+        /// PaymentAmount of the ExplanationOfBenefits 
+        /// </summary>
+        public int? PaymentAmount { get; set; }
+
+        /// <summary>
+        /// CreatedOn of the ExplanationOfBenefits 
+        /// </summary>
+        [Column(TypeName = "datetime")]
+        public DateTime? CreatedOn { get; set; }
+        /// <summary>
+        /// CreatedBy of the ExplanationOfBenefits 
+        /// </summary>
+        public Guid? CreatedBy { get; set; }
+
+        /// <summary>
+        /// UpdatedOn of the ExplanationOfBenefits 
+        /// </summary>
+        [Column(TypeName = "datetime")]
+        public DateTime? UpdatedOn { get; set; }
+        /// <summary>
+        /// UpdatedBy of the ExplanationOfBenefits 
+        /// </summary>
+        public Guid? UpdatedBy { get; set; }
+    }
+}
